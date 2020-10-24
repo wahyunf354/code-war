40a4e7b387325e4115f9a5dcf943a592a2967ce2
wahyunf354 <wahyunurfadillah313@gmail.com>
2020-10-24 15:58:07 +0700

    fix EvenOrOdd and ReturningString

---
+++ 8kyu/EvenOrOdd/index.js
@@ -0,0 +1,6 @@
+const even_or_odd = (number) => (number % 2 ? "Odd" : "Even");
+
+console.log(even_or_odd(2), "Even");
+console.log(even_or_odd(0), "Even");
+console.log(even_or_odd(7), "Odd");
+console.log(even_or_odd(1), "Odd");
---
+++ 8kyu/ReturningStrings/index.js
@@ -0,0 +1,4 @@
+const greet = (name) => `Hello, ${name} how are you doing today?`;
+
+console.log(greet("Ryan"), "Hello, Ryan how are you doing today?");
+console.log(greet("Shingles"), "Hello, Shingles how are you doing today?");
