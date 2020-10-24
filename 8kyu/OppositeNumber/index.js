4c6921178c557339c70fa916e0af8f913daf30dc
wahyunf354 <wahyunurfadillah313@gmail.com>
2020-10-24 15:28:59 +0700

    fix oppositeNumber

---
+++ 8kyu/OppositeNumber/index.js
@@ -0,0 +1,9 @@
+/**
+ * @param {number} number
+ * @decription Mengubah angka positif menjadi negatif dan sebaliknya
+ * @example 1 => -1
+ */
+
+const opposite = (number) => -number;
+
+console.log(opposite(1), -1);
