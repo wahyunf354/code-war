f5576ddc62366e920b875d0840c052856b68f548
wahyunf354 <wahyunurfadillah313@gmail.com>
2020-10-25 17:23:58 +0700

    fix remove first and las char

--- 8kyu/RemoveFirstAndLastChar/index.js
+++ 8kyu/RemoveFirstAndLastChar/index.js
@@ -1,4 +1,10 @@
-const removeChar = (str) => {};
+const removeChar = (str) => {
+  // let split = str.split("");
+  // split.pop();
+  // split.shift();
+  // return split.join("");
+  return str.substring(1, str.length - 1);
+};
 
 console.log(removeChar("eloquent"), "loquen");
 console.log(removeChar("country"), "ountr");
