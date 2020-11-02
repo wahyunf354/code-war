3345ff92b3c5819595a2fe29420f4606c1298e28
wahyunf354 <wahyunurfadillah313@gmail.com>
2020-11-02 11:19:52 +0700

    fix ArrayPlusArray

---
+++ 8kyu/ArrayPlusArray/index.js
@@ -0,0 +1,9 @@
+function arrayPlusArray(arr1, arr2) {
+  const reducer = (accumulator, currentValue) => accumulator + currentValue;
+  return arr1.reduce(reducer) + arr2.reduce(reducer);
+}
+
+console.log(arrayPlusArray([1, 2, 3], [4, 5, 6]), 21);
+console.log(arrayPlusArray([-1, -2, -3], [-4, -5, -6]), -21);
+console.log(arrayPlusArray([0, 0, 0], [4, 5, 6]), 15);
+console.log(arrayPlusArray([100, 200, 300], [400, 500, 600]), 2100);
